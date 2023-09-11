35a896af737c3763432e5856afce7e0718211197
Manuel González Villegas <manuelgon47@gmail.com>
2023-09-11 16:38:47 +0200

    Initial commit with the project creation

--- Package.swift
+++ Package.swift
@@ -6,7 +6,13 @@ import CompilerPluginSupport
 
 let package = Package(
     name: "playtomic-macros",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+    platforms: [
+//        .macOS(.v10_15),
+        .iOS(.v13)
+//        .tvOS(.v13),
+//        .watchOS(.v6),
+//        .macCatalyst(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -20,7 +26,10 @@ let package = Package(
     ],
     dependencies: [
         // Depend on the latest Swift 5.9 prerelease of SwiftSyntax
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0-swift-5.9-DEVELOPMENT-SNAPSHOT-2023-04-25-b"),
+        .package(
+            url: "https://github.com/apple/swift-syntax.git",
+            from: "509.0.0-swift-5.9-DEVELOPMENT-SNAPSHOT-2023-04-25-b"
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
