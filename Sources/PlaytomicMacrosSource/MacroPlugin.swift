a0950fee56b79b8c27c99d76828dd347dac48d4c
mohamad reza koohkan <mohamad_koohkan@icloud.com>
2024-07-22 22:23:44 +0300

    Disable all macros except Copyable

--- Sources/PlaytomicMacrosSource/MacroPlugin.swift
+++ Sources/PlaytomicMacrosSource/MacroPlugin.swift
@@ -4,16 +4,16 @@ import SwiftSyntaxMacros
 @main
 struct MacrosPlugin: CompilerPlugin {
   let providingMacros: [Macro.Type] = [
-    StringifyMacro.self,
-    WarningMacro.self,
-    URLMacro.self,
-    AddAsyncMacro.self,
-    CaseDetectionMacro.self,
-    CopyableMacro.self,
-    WrapStoredPropertiesMacro.self,
-    StoredAccessMacro.self,
-    EquatableMacro.self,
-    SealedMacro.self,
-    AddInitMacro.self
+//    StringifyMacro.self,
+//    WarningMacro.self,
+//    URLMacro.self,
+//    AddAsyncMacro.self,
+//    CaseDetectionMacro.self,
+    CopyableMacro.self
+//    WrapStoredPropertiesMacro.self,
+//    StoredAccessMacro.self,
+//    EquatableMacro.self,
+//    SealedMacro.self,
+//    AddInitMacro.self
   ]
 } 
